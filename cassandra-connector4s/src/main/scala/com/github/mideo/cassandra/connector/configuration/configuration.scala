4fc8b5ad65371c8a4787045374152d733fb69202
Mide Ojikutu <mide.ojikutu@gmail.com>
2019-03-25 21:14:00 +0000

    clean up

--- cassandra-connector4s/src/main/scala/com/github/mideo/cassandra/connector/configuration/configuration.scala
+++ cassandra-connector4s/src/main/scala/com/github/mideo/cassandra/connector/configuration/configuration.scala
@@ -12,8 +12,8 @@ package object configuration {
       with Environment {
 
     val config: Config = _env.get("cassandra.connector.environment.conf") match {
-      case None => updateFromEnvVars(ConfigFactory.load("cassandra-connector.conf"))
-      case Some(value) => updateFromEnvVars(ConfigFactory.load(s"cassandra-connector.$value.conf"))
+      case None => ConfigFactory.load("cassandra-connector.conf")
+      case Some(value) => ConfigFactory.load(s"cassandra-connector.$value.conf")
     }
   }
 
@@ -44,24 +44,5 @@ package object configuration {
         case _: ConfigException => None
       }
     }
-
-    protected def updateFromEnvVars(conf: Config): Config = {
-      val temp: mutable.Map[String, Any] = mutable.Map.empty
-
-      conf.entrySet().forEach(
-        entry => {
-          val _key = entry.getKey
-          val sysKey = _key.toUpperCase.replaceAll("(\\.|-)", "_")
-
-          _env.get(sysKey) match {
-            case None => temp.put(_key, ConfigValueFactory.fromAnyRef(conf.getAnyRef(_key)))
-            case Some(value) => temp.put(_key, ConfigValueFactory.fromAnyRef(value))
-          }
-        }
-      )
-      ConfigFactory.parseMap(temp.asJava)
-    }
   }
-
-
 }
