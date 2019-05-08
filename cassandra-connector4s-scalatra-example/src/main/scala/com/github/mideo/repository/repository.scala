dd5c073a8720017dba971b74c3178cf267bf6116
Mide Ojikutu <mide.ojikutu@gmail.com>
2019-05-08 17:01:47 +0100

    update example

--- cassandra-connector4s-scalatra-example/src/main/scala/com/github/mideo/repository/repository.scala
+++ cassandra-connector4s-scalatra-example/src/main/scala/com/github/mideo/repository/repository.scala
@@ -4,7 +4,6 @@ import com.datastax.driver.core.ConsistencyLevel
 import com.github.mideo.cassandra.connector.fluent.Connector
 import com.github.mideo.cassandra.connector.repository.ConnectedKeyspace
 import com.github.mideo.cassandra.testing.support.EmbeddedCassandra
-import com.sun.nio.zipfs.ZipFileSystemProvider
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -14,23 +13,15 @@ package object repository {
   EmbeddedCassandra.startDb
 
   val CassandraKeyspace: Future[ConnectedKeyspace] = for {
-      c <- Future {
-        Connector
-          .keyspace("cassandra_connector")
-          .onPort(EmbeddedCassandra.runningPort)
-          .withConsistencyLevel(ConsistencyLevel.LOCAL_ONE)
-          .withContactPoints(EmbeddedCassandra.getHosts)
-          .connect()
-      }
-      _ <- {
-        val uri =  this.getClass.getClassLoader.getResource("migrations").toURI
-        if(new ZipFileSystemProvider().getScheme.equals(uri.getScheme)) {
-          import java.nio.file.FileSystems
-          import collection.JavaConverters._
-          FileSystems.newFileSystem(uri, Map("create" ->"true").asJava)
-        }
-        c.runMigrations("migrations")
-      }
-    } yield c
+    c <- Connector
+      .keyspace("cassandra_connector")
+      .onPort(EmbeddedCassandra.runningPort)
+      .withConsistencyLevel(ConsistencyLevel.LOCAL_ONE)
+      .withContactPoints(EmbeddedCassandra.getHosts)
+      .connect()
+    _ <- {
+      c.runMigrations("migrations")
+    }
+  } yield c
 
 }
